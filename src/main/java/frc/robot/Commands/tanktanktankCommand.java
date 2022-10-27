31dbb0a32db1dbc1a50c227ba34efe82339862b4
InterstellarDrift <InterstellarDrift@github.com>
2022-10-27 18:28:24 -0400

    initial command/subsystem setup

---
+++ src/main/java/frc/robot/Commands/tanktanktankCommand.java
@@ -0,0 +1,36 @@
+package frc.robot.Commands;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Subsystems.tanktanktankSubsystem;
+
+public class tanktanktankCommand extends CommandBase {
+    @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+    private final tanktanktankSubsystem tanktanktanksubsystem;
+    private final Joystick leftStick;
+    private final Joystick rightStick;
+
+    /**
+     * Creates a new ExampleCommand.
+     *
+     * @param subsystem The subsystem used by this command.
+     */
+    public tanktanktankCommand(tanktanktankSubsystem subsystem, Joystick leftstick, Joystick rightstick) {
+        tanktanktanksubsystem = subsystem;
+        leftStick = leftstick;
+        rightStick = rightstick;
+        // Use addRequirements() here to declare subsystem dependencies.
+        addRequirements(subsystem);
+
+    }
+    @Override
+    public void initialize(){
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
+
+}
---
+++ src/main/java/frc/robot/Subsystems/tanktanktankSubsystem.java
@@ -0,0 +1,37 @@
+package frc.robot.Subsystems;
+
+import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class tanktanktankSubsystem extends SubsystemBase {
+//    Joystick m_leftStick = new Joystick(0);
+//    Joystick m_rightStick = new Joystick(1);
+    private VictorSPX frontleft = new VictorSPX(12);
+    private VictorSPX backleft = new VictorSPX(13);
+//    backleft.follow(frontleft);
+    private VictorSPX frontright = new VictorSPX(10);
+    private VictorSPX backright = new VictorSPX(11);
+
+    public tanktanktankSubsystem() {
+        super();
+        backleft.follow(frontleft);
+        backright.follow(frontright);
+    }
+
+    public void tankDrive(double left, double right) {
+        frontleft.set(VictorSPXControlMode.PercentOutput, left);
+        frontright.set(VictorSPXControlMode.PercentOutput, -right);
+
+    }
+    @Override
+    public void periodic() {
+
+        //this method will be called once per scheduler run
+    }
+    @Override
+    public void simulationPeriodic() {
+        //will be called once per scheduler run during simulation
+    }
+}
